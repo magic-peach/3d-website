d3627af881fb0d6cfd7cb78b232f54c7e165d8bf
magic-peach <akankshatrehun@gmail.com>
2024-08-09 23:28:11 +0530

    added popups

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+const renderContent = {
+  1:(
+    <h1>1</h1>
+  ),
+  2:(
+    <h1>2</h1>
+  ),
+  3:(
+    <h1>3</h1>
+  ),
+  4:(
+    <h1>4</h1>
+  ),
+}
 
-const HomeInfo = () => {
-  return (
-    <div>HomeInfo</div>
-  )
+const InfoBox = ({text,link, btnText}) => (
+  <div>
+
+  </div>
+)
+
+const HomeInfo = ({currentStage}) => {
+  return renderContent [currentStage] || null;
 }
 
 export default HomeInfo
